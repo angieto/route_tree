176e88bf2fb23b5a311a245df273e31f1d9425e7
Angie To <angie.to@berkeley.edu>
2018-09-18 15:54:21 -0700

    fully functional

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
+import { AllComponent } from './reviews/all/all.component';
+import { AuthorComponent } from './reviews/author/author.component';
+import { CategoryComponent } from './products/category/category.component';
+import { BrandComponent } from './products/brand/brand.component';
+import { DetailsComponent } from './reviews/details/details.component';
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent, children: [
+        { path: 'details/:id', component: DetailsComponent},
+        { path: 'brand/:brand', component: BrandComponent},
+        { path: 'category/:cat', component: CategoryComponent}
+    ] },
+    { path: 'reviews', component: ReviewsComponent, children: [
+        { path: 'details/:id', component: DetailsComponent},
+        { path: 'author/:id', component: AuthorComponent},
+        { path: 'all/:id', component: AllComponent}
+    ] },
+    { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouteTreeService } from './route-tree.service';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { CategoryComponent } from './products/category/category.component';
+import { BrandComponent } from './products/brand/brand.component';
+import { DetailsComponent } from './reviews/details/details.component';
+import { AuthorComponent } from './reviews/author/author.component';
+import { AllComponent } from './reviews/all/all.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ReviewsComponent,
+    ProductDetailComponent,
+    CategoryComponent,
+    BrandComponent,
+    DetailsComponent,
+    AuthorComponent,
+    AllComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RouteTreeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
